d2ee26d5fb0d0412cc1e642c82225cff42b28f48
GunterG-1 <gu.garate@duocuc.cl>
2025-05-15 02:18:33 -0400

    Add files via upload

---
+++ Eckomark/Eckomark/src/main/java/com/Ecomark_vm/Eckomark/EckomarkApplication.java
@@ -0,0 +1,13 @@
+package com.Ecomark_vm.Eckomark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EckomarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EckomarkApplication.class, args);
+	}
+
+}
---
+++ Eckomark/Eckomark/src/test/java/com/Ecomark_vm/Eckomark/EckomarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecomark_vm.Eckomark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EckomarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
